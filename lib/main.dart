076af7fba430b576514fd09167493ae52548104e
arbre <nayeem@arbreesolutions.com>
2025-02-04 15:53:37 +0600

    asas

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,26 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-import 'ad_helper.dart';
-
+// Note: User code for main function shown in step-2
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  RequestConfiguration requestConfiguration = RequestConfiguration(
-    testDeviceIds: ['2DB5553B338D712077B14562898A1C1D'], // Replace with your test device ID
-  );
-  MobileAds.instance.updateRequestConfiguration(requestConfiguration);
-
-  MobileAds.instance.initialize(); // Initialize after setting configuration
-
-  runApp(MyApp());
+  MobileAds.instance.initialize();
+  runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Google Admob Rewarded Ads',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Google Admob Rewarded Ads'),
     );
   }
 }
@@ -42,86 +35,105 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  BannerAd? _bannerAd;
-  final request = AdRequest(
-    keywords: ['foo', 'bar'],
-    nonPersonalizedAds: true,
-  );
+  RewardedAd? _rewardedAd;
+  var rewardPoints = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   void initState() {
+// TODO: implement initState
     super.initState();
-    Future.delayed(Duration(seconds: 2), () {
-      _bannerAd = BannerAd(
-        adUnitId: AdMobService.bannerAdUnitId!,
-        request: request,
-        size: AdSize.banner,
-        listener: BannerAdListener(
-          onAdLoaded: (ad) {
-            setState(() {
-              _bannerAd = ad as BannerAd;
-            });
-          },
-          onAdFailedToLoad: (ad, err) {
-            print("Failed To Load: $err ${ad.responseInfo}");
-            ad.dispose();
-          },
-        ),
-      )..load();
-    });
+    loadAd();
   }
 
-  @override
-  void dispose() {
-    _bannerAd?.dispose();
-    super.dispose();
+  final adUnitId = Platform.isAndroid
+      ? 'ca-app-pub-3940256099942544/5224354917'
+      : 'ca-app-pub-3940256099942544/1712485313';
+
+  void loadAd() {
+    RewardedAd.load(
+        adUnitId: adUnitId,
+        request: const AdRequest(),
+        rewardedAdLoadCallback: RewardedAdLoadCallback(onAdLoaded: (ad) {
+          ad.fullScreenContentCallback = FullScreenContentCallback(
+              onAdShowedFullScreenContent: (ad) {},
+              onAdImpression: (ad) {},
+              onAdFailedToShowFullScreenContent: (ad, err) {
+                ad.dispose();
+              },
+              onAdDismissedFullScreenContent: (ad) {
+                ad.dispose();
+              },
+              onAdClicked: (ad) {});
+
+          _rewardedAd = ad;
+        }, onAdFailedToLoad: (LoadAdError error) {
+          print('RewardedAd failed to load: $error');
+        }));
   }
 
+  //Note:- Use code here for variable declaration, load and show ads from step-2
+  void showAds() {
+    _rewardedAd?.show(
+        onUserEarnedReward: (AdWithoutView ad, RewardItem rewardItem) {
+          // Reward the user for watching an ad.
+          print("Reward points:- ${rewardItem.amount}");
+          rewardPoints+= rewardItem.amount.toInt();
+          setState(() {
+            rewardPoints;
+            print("Reward:- $rewardPoints");
+          });
+        });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
         title: Text(widget.title),
+        centerTitle: Platform.isAndroid ? false : true,
       ),
-      body: Stack(
-        children: [
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'You have pushed the button this many times:',
-                ),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(height: 100),
+            const Text(
+              'You will get 10 points for watching video ads.',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600,color: Colors.black),
             ),
-          ),
-          if (_bannerAd != null)
-            Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                width: _bannerAd!.size.width.toDouble(),
-                height: _bannerAd!.size.height.toDouble(),
-                child: AdWidget(ad: _bannerAd!),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: Image.asset(
+                "images/reward.png",
+                fit: BoxFit.fill,
               ),
             ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+            const SizedBox(height: 20),
+            Text(
+              'Rewards Points:- $rewardPoints',
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(height: 50),
+            Container(
+              width: 100,
+              height: 50,
+              color: Colors.blue,
+              child: TextButton(
+                  onPressed: () {
+                    showAds();
+                  },
+                  child: const Text('Show Ads',
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white))),
+            )
+          ],
+        ),
       ),
     );
   }
